701f22199ed9efe423d8e5e74e8f711fe4bc8020
Ujjawal Srivastava <srivastava.ujjawal011@gmail.com>
2021-07-27 01:07:29 +0530

    Deploying to Heroku Server

--- todolist_project/settings.py
+++ todolist_project/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-qop(wam&elfa6id^%$umsz&i26a_$&mfa!o_%emgs8yrwe_u3('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://almorex-todo-list.herokuapp.com/', '127.0.0.1']
 
 
 # Application definition
